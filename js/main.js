99e93849c4e1f91eb5d886d8c1fc94c44caa87de
muhammadjewel <msuratov@gmail.com>
2023-08-18 21:30:11 +0500

    Add img and JSON data

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+    "category": "Reaction",
+    "score": 80,
+    "icon": ".img/icon-reaction.svg"
+  },
+  {
+    "category": "Memory",
+    "score": 92,
+    "icon": ".img/icon-memory.svg"
+  },
+  {
+    "category": "Verbal",
+    "score": 61,
+    "icon": ".img/icon-verbal.svg"
+  },
+  {
+    "category": "Visual",
+    "score": 72,
+    "icon": ".img/icon-visual.svg"
+  }
+];
